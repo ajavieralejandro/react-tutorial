c993566e275db9a6a75aaf0f5260620bc3b9470d
ajavieralejandro <amorosijavier@gmail.com>
2020-02-03 13:55:54 -0300

    adding react lazy and suspense ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React , {useEffect} from "react";
+import React , {useEffect,lazy, Suspense} from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {checkUserSession } from "./redux/user/user.actions";
 //proob comment
 //Firebase
 
+
 import {GlobalStyle} from "./global.styles";
-import CheckOutPage from "./pages/checkout/checkout.component";
 
 //Solo lo ejecuta una ves
 const App = ({checkUserSession,currentUser}) =>  {
+  //lazy loading 
+
+  const ShopPage = lazy(()=> import('./pages/shop/shop.component'));
+  const CheckOutPage = lazy(()=> import('./pages/checkout/checkout.component'));
+  const SignInAndSignUpPage = lazy(()=> import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
 
   useEffect( () => {
     checkUserSession();
@@ -38,9 +41,11 @@ const App = ({checkUserSession,currentUser}) =>  {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Suspense fallback={<div>...Loading</div>}>
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckOutPage} />
           <Route exact path="/signin" render={handleRedirect} />
+          </Suspense>
         </Switch>
       </div>
     );
